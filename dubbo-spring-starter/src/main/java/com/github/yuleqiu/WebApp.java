251650915498c8dcab99fb969254977622d39e93
gc_chenqiu <gc_chenqiu@huoyunren.com>
2017-08-09 15:36:20 +0800

    日志级别定义

--- dubbo-spring-starter/src/main/java/com/github/yuleqiu/WebApp.java
+++ dubbo-spring-starter/src/main/java/com/github/yuleqiu/WebApp.java
@@ -1,12 +1,11 @@
 package com.github.yuleqiu;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
-public class WebApp extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(App.class);
-    }
+public class WebApp /* extends SpringBootServletInitializer */{
+    // @Override
+    // protected SpringApplicationBuilder configure(SpringApplicationBuilder
+    // application) {
+    // return application.sources(App.class);
+    // }
 }
